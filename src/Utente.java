55a157cd5554f38e6141840d0e86a5700eaefa1a
Francisco <franciscomarcelo4001@gmail.com>
2024-09-24 14:47:49 +0100

    Added gets and sets. version 2

--- src/Utente.java
+++ src/Utente.java
@@ -50,5 +50,69 @@ public class Utente {
         this.peso = peso;
         instanceNumber++;
     }
+    public static void setInstanceNumber(int instanceNumber) {
+        Utente.instanceNumber = instanceNumber;
+    }
+
+    public static int getState1() {
+        return state1;
+    }
+
+    public static void setState1(int nstate1) {
+        state1 = nstate1;
+    }
+
+    public static int getState2() {
+        return state2;
+    }
+
+    public static void setState2(int nstate2) {
+        state2 = nstate2;
+    }
+
+    public static int getInstanceNumber() {
+        return instanceNumber;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getGenero(){
+        return genero;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public double getAltura(){
+        return altura;
+    }
+
+    public double getPeso(){
+        return peso;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
 
 }
